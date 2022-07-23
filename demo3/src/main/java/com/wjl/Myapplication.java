f1a3f45111e5c7380cbff6db0a8446903d67ae95
wjl <1835633835@qq.com>
2022-07-23 16:04:22 +0800

    one

---
+++ demo3/src/main/java/com/wjl/Myapplication.java
@@ -0,0 +1,12 @@
+package com.wjl;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Myapplication.class,args);
+    }
+}
